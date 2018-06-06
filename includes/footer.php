518dbb23b1d5baef3cb859e500240be26eefb36e
rusev9 <evg.rusev@gmail.com>
2018-06-06 00:38:22 -0700

    optimised dinamic content by using includes includes

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<section id="footer">
+      <div class="container">
+            <div class="footer-content">
+              <p id="footer-copyright">
+                © Copyright 2018 <a href="#">Rusev Web Services</a> - All Rights Reserved
+              </p>
+            </div>
+      </div>
+  </section>
+</body>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<body class="container">
+  <header>
+    <div class="container">
+      <div id="branding">
+        <img src="img/rusev-logo.png" alt="R logo">
+      </div>
+      <nav>
+        <ul>
+          <li class="current"><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="services.html">Services</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="bottom-header">
+      <div class="text-center">
+        <h1>Rusev Web Solutions</h1>
+      </div>
+      <div class="sub-text-center">
+        <h2>Rusev Web Solutions</h2>
+      </div>
+    </div>
+  </header>
