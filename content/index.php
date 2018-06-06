6039e856221de1d6fbf79877965b94a9dbdf506a
rusev9 <evg.rusev@gmail.com>
2018-06-06 00:25:21 -0700

    developed services section + developed footer

---
+++ content/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <meta name="description" content="Web development and internet marketing">
+  <meta name="keywords" content="web services, web development">
+  <meta name="author" content="Evgeni Rusev">
+  <title>Rusev Web Solutions &#8211; Web Development Services</title>
+  <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+</head>
+<body class="container">
+  <header>
+    <div class="container">
+      <div id="branding">
+        <img src="img/rusev-logo.png" alt="R logo">
+      </div>
+      <nav>
+        <ul>
+          <li class="current"><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="services.html">Services</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="bottom-header">
+      <div class="text-center">
+        <h1>Rusev Web Solutions</h1>
+      </div>
+      <div class="sub-text-center">
+        <h2>Rusev Web Solutions</h2>
+      </div>
+    </div>
+  </header>
+  <section id="showcase">
+    <div class="container">
+      <div class="showcase-title">
+        <h3 id="first-showcase-title">Web Development And Digital Marketing</h3>
+      </div>
+      <div class="grid">
+        <div class="showcase-img">
+          <img src="img/pc.png" alt="computer and books">
+        </div>
+        <div class="showcase-p">
+          <p id="first-showcase-p">Today's digital marketplace is growing fast. It's noisy and volatile out there, but you can stand out from the competition. Our services can help you connect with your customers in a more efficient and smart way. We provide our customers with affordable digital solutions for their businesses and projects.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="services-section">  
+          <h3>Services</h3>
+          <div class="boxes">
+            <div class="box">
+              <img src="https://achievement-images.teamtreehouse.com/badges_WP_SEO_Stage2.png">
+              <h3>Web Design</h3>
+              <p>We strive to create just the right website for each client. We can help you have the website that will not only look great, but meet the needs of both you and your target audience.</p>
+            </div>
+            <div class="box">
+              <img src="https://achievement-images.teamtreehouse.com/badges_WP_AdminSettings_Stage1.png">
+              <h3>Website Management</h3>
+              <p>We offer any services in regards to website management and solving issues related to website functionality or bugs.</p>
+            </div>      
+            <div class="box">
+              <img src="https://achievement-images.teamtreehouse.com/badges_html_howtomakeawebsite_stage08.png">
+              <h3>Mobile Friendliness</h3>
+              <p>Statistics suggest that over 54% of the internet traffic nowdays comes from mobile devices. We provide our clients with responsive website optimisation which will surely increase the engagement with their clients.</p>
+            </div>
+            <div class="seccond-boxes">
+              <div class="box">
+                <img src="https://achievement-images.teamtreehouse.com/badges_WP_woocommerce_Stage1.png">
+                <h3>Internet Marketing</h3>
+                <p>We provide online advertisement management services. We can help you promote your business in new creative and smart ways. We have experience in running paid advertisement campaigns such as Google Adsense and Facebook Ads.</p>
+              </div>
+              <div class="box">
+                <img src="https://achievement-images.teamtreehouse.com/badges_MarketingBiz_Stage1.png">
+                <h3>Social Media Management</h3>
+                <p>Social media has undoubtedly become a tremendously influential aspect of the internet world. We find that to be an opportunity to connect our clients to their customers by developing and managing social signals for their businesses such as Facebook pages, Pinterest groups, Instagram accounts etc.</p>
+              </div>
+              <div class="box">
+                <img src="https://achievement-images.teamtreehouse.com/badges_DD_bizblogging_stage3.png">
+                <h3>Monetization</h3>
+                <p>We offer smart and efficient monetization strategies such as PPC, CPA and Affiliate Marketing. We are experienced in analising statistical traffic data which lets us optimize our strategy to achieve maximum monetization efficiency.</p>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </section>
+  <section id="footer">
+      <div class="container">
+            <div class="footer-content">
+              <p id="footer-copyright">
+                © Copyright 2018 <a href="#">Rusev Web Services</a> - All Rights Reserved
+              </p>
+            </div>
+      </div>
+  </section>
+</body>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php
-include realpath("content/index.html");
+include realpath("content/index.php");
