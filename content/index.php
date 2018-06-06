8628f9e1d5f3bee4f03baf52d24c5156b1bcc3e6
rusev9 <evg.rusev@gmail.com>
2018-06-06 01:50:21 -0700

    added contact us form and styling

--- content/index.php
+++ content/index.php
@@ -3,4 +3,5 @@ include 'includes/head.php';
 include 'includes/header.php';
 include 'includes/intro.php';
 include 'includes/services.php';
+include 'includes/form.php';
 include 'includes/footer.php';
