ae6c5ca2c08a405ff866608d3f58489be4c84d6c
rusev9 <evg.rusev@gmail.com>
2018-06-15 02:42:42 -0700

    rollback Rapid-Authentication

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="{{route('posts.index')}}"><span style="font-size:40px">Branding</span></a>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #ffffff;">
+  <!-- <nav class="navbar navbar-light" style="background-color: #e3f2fd;"> -->
+  <a class="navbar-brand" href="{{route('posts.index')}}"><span style="font-size:40px">Laravel Forum</span></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -16,32 +17,5 @@
       </li>
     </ul>
   </div>
-  <div>
-    <ul class="nav navbar-nav navbar-right">
-      <a href="{{route('posts.create')}}" class="btn btn-primary">Ask A Question</a>
-      @guest
-      <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-      <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-      @else
-      <li class="nav-item dropdown">
-        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-          {{ Auth::user()->name }} <span class="caret"></span>
-        </a>
-
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="{{ route('logout') }}"
-          onclick="event.preventDefault();
-          document.getElementById('logout-form').submit();">
-          {{ __('Logout') }}
-        </a>
-
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-          @csrf
-        </form>
-      </div>
-    </li>
-    @endguest
-  </ul>
-</div>
 </nav>
 <br>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
-
 Route::resource('posts', 'PostsController');
 Route::resource('comments', 'CommentsController');
 
 Route::get('/', 'PostsController@index');
 Route::get('contact', function() { return view('contact'); })->name('contact');
 Route::get('about', function() { return view('about'); })->name('about');
-
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
