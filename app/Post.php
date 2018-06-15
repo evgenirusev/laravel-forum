e18f2887588275d09bf1651a22b58133ab27a5a7
rusev9 <evg.rusev@gmail.com>
2018-06-14 20:02:16 -0700

    Add model one-to-many relationship definition

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    public function comments() {
+    public function comments()
+    {
       return $this->hasMany('\App\Comment');
+    }
+
+    public function user()
+    {
+      return $this->belongsTo('\App\User');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('posts', 'PostsController');
+Route::resource('comments', 'CommentsController');
 
 Route::get('/', 'PostsController@index');
-Route::resource('posts', 'PostsController');
-
 Route::get('contact', function() { return view('contact'); })->name('contact');
 Route::get('about', function() { return view('about'); })->name('about');
-
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
