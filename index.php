139334ce0bcade97f09621de74a407dd9c7d4efc
rusev9 <evg.rusev@gmail.com>
2018-06-22 16:16:49 +0300

    first commit

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header("Location: http://intersolutions.ml/public/");
-exit();
--- resources/views/default.blade.php
+++
@@ -1,9 +0,0 @@
-@include('layouts/head')
-<body>
-  @include('layouts/navbar')
-  <div class="container">
-    @yield('content')
-  </div>
-  @include('layouts/footer')
-</body>
-</html>
